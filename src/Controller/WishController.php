eb9b74a027275e751b1cd364b654d7acbe897b43
Luca DUPONT <luca.dupont@orange.fr>
2023-02-23 11:07:11 +0100

    TP Doctrine

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,16 +11,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class WishController extends AbstractController
 {
     #[Route('/list', name: 'list')]
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
-        return $this->render('wish/list.html.twig');
+
+        $wishes = $wishRepository->findAll();
+
+        return $this->render('wish/list.html.twig',["wishes"=>$wishes]);
     }
 
     #[Route('/{id}', name: 'show' , requirements: ["id" => "\d+"])]
-    public function detail($id): Response
+    public function detail(int $id, WishRepository $wishRepository): Response
     {
-        dump($id);
-        return $this->render('wish/detail.html.twig');
+
+        $wish = $wishRepository->find($id);
+
+        return $this->render('wish/detail.html.twig',["wish"=>$wish]);
     }
 
 
